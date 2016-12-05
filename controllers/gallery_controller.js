de7208ca09bffa386d714f3c3c209aedb20afe41
Alberto <apascual93@gmail.com>
2016-12-05 17:31:21 +0100

    new support

--- controllers/gallery_controller.js
+++ controllers/gallery_controller.js
@@ -2,6 +2,16 @@ var fs = require('fs');
 var gallery_model = require('./../models/gallery.js');
 var watson = require('watson-developer-cloud');
 
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: 'HCy6msZTwP9ObW9lWms8i1Md1',
+  consumer_secret: 'Eh9RFBx7zu4JGzGGHJNCS05zBN8XbS1e8yr2l77VPr3aAAbW2u',
+  access_token_key: '377869454-x1QxQSdB69bpoeo2nuoYafiu5A7by4j8MIGpCe9r',
+  access_token_secret: 'y1ZLoWq43yoaP91E0Htg8NST2CVmwuPURXfGuaTDgWAZl'
+});
+
+
 exports.list = function (req, res) {
 	var images = gallery_model.photos;
 	console.log(images)
@@ -18,13 +28,14 @@ exports.new = function (req, res) {
 exports.show = function (req, res) {
 	var image = gallery_model.photos[req.params.photoId];
 	var analysis = undefined
+	var twitterprofile = undefined
 	//console.log(image.url)
 	var visual_recognition = watson.visual_recognition({
 	  api_key: '73ab782e6b49d418a6e5706fa487b80e1cf2f21a',
 	  version: 'v3',
 	  version_date: '2016-05-20'
 	});
-
+	console.log("processing...")
 	var params = {
 	  images_file: fs.createReadStream('public'+image.url)
 	};
@@ -38,8 +49,28 @@ exports.show = function (req, res) {
 		//JSON.parse(analysis)
 		console.log(analysis.images[0].faces[0])
 		console.log(typeof analysis)
-		res.render('gallery/show', {image: image, age: analysis.images[0].faces[0].age, gender: analysis.images[0].faces[0].gender,
-		identity: analysis.images[0].faces[0].identity});	
+
+		if (analysis.images[0].faces[0].identity != undefined) {
+			//Search for social profiles
+			var dname = analysis.images[0].faces[0].identity.name;
+			client.get('users/search', {q: dname, include_entities: false}, function(error, users, response) {
+		    //console.log(users);
+		    for (var i = 0; i<users.length; i++) {
+		   	  if (users[i].verified == true){
+		   	 	twitterprofile = users[i].screen_name
+		   	 	console.log(twitterprofile)
+		   	 	res.render('gallery/show', {image: image, profiles: twitterprofile, age: analysis.images[0].faces[0].age, gender: analysis.images[0].faces[0].gender,
+				identity: analysis.images[0].faces[0].identity});
+		   	  }
+		    }
+			});
+
+		}
+		else{
+		console.log("profile: "+twitterprofile)
+		res.render('gallery/show', {image: image, profiles: undefined, age: analysis.images[0].faces[0].age, gender: analysis.images[0].faces[0].gender,
+		identity: analysis.images[0].faces[0].identity});
+		}
 	});
 	
 };
@@ -50,7 +81,7 @@ exports.create = function (req, res) {
 	console.log('Nuevo fichero de imagen. Datos: ', image);
 	console.log(image.originalname)
 	var imgname = image.originalname.split('.')[0];
-	// Aquí debe implementarse la escritura del fichero de audio (track.buffer) en tracks.cdpsfy.es
+	
 	fs.writeFile('public/players/'+image.originalname, image.buffer, function(err) {
         if(err) {
     		return console.log(err);
@@ -62,7 +93,7 @@ exports.create = function (req, res) {
 
 	console.log(imgurl);
 
- 	// Escribe los metadatos de la nueva canción en el registro.
+ 	
 	gallery_model.photos[imgname] = {
 		name: imgname,
 		url: imgurl
